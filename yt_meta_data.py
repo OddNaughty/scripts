b8901b549a885cff0501e9fb4a8dc89d757b51df
Camille WAGNER <cwagner@e3r9p18.42.fr>
2016-03-11 16:38:28 +0100

    Can change the youtube path in script

--- yt_meta_data.py
+++ yt_meta_data.py
@@ -1,15 +1,19 @@
 import argparse
 from MyMusic import MyMusic
 
-def main(url):
+def main(url, path):
     """
     Requirements: youtube_dl, mutagen
     """
-    mytag = MyMusic()
+    if not path:
+        mytag = MyMusic()
+    else:
+        mytag = MyMusic(path)
     mytag.get_song_from_yt(url)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to download a youtube/soundcloud video and convert to mp3")
     parser.add_argument('url')
+    parser.add_argument('-yt_path', help="The directory to store the converted files")
     args = parser.parse_args()
-    main(args.url)
+    main(args.url, args.yt_path)
